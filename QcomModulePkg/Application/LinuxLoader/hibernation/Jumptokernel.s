85fa2ed5195e8f83aa223e470649bde5d9c0cb6f
Shreyas K K <shrekk@codeaurora.org>
2020-01-17 13:18:07 +0530

    Hibernation: Relocate page tables and JumpToKernel
    ABL copies bounced pages after UEFI exit. To aid performance, MMU is
    kept on during this period. There is a possibility of kernel pages
    conflicting with page tables and end up corrupting them. Fix this
    potential page table corruption by copying the page tables
    to a new location unused by kernel.
    
    The JumpToKernel code (which will restore the bounce pages and
    transfer control to kernel) is also relocated to a unused memory
    location which does not  conflict with kernel pages.
    
    Change-Id: I6dcf84a1e9fde6592c1191463c842e65a223a24a
    Signed-off-by: Shreyas K K <shrekk@codeaurora.org>

--- QcomModulePkg/Application/LinuxLoader/hibernation/Jumptokernel.s
+++ QcomModulePkg/Application/LinuxLoader/hibernation/Jumptokernel.s
@@ -32,7 +32,7 @@
  * size of single entry in table
  * currently src & dst pfn are 8 bytes each.
  */
-#define	ENTRY_SIZE			16
+#define	ENTRY_SIZE		16
 
 /*
  * ENTRIES_PER_TABLE = (PAGE_SIZE / ENTRY_SIZE) - 1
@@ -41,11 +41,11 @@
 #define	ENTRIES_PER_TABLE	255
 
 .globl JumpToKernel;
+
 /*
  * x18 = bounce_pfn_entry_table
  * x19 = bounce_count
  * x21 = cpu_resume
- * x22 = PreparePlatformHardware
  */
 JumpToKernel:
 	mov	x4, #ENTRIES_PER_TABLE
@@ -59,7 +59,7 @@ JumpToKernel:
 	madd	x0, x1, x2, x18			// x0 = x1 * x2 + x18
 	ldr	x18, [x0]			// load address of next table
 	cbnz	x19, JumpToKernel      		// loop until bounce_count equals 0
-	blr	x22				// call PreparePlatformHardware
+	bl	_PreparePlatformHardware	// call PreparePlatformHardware
 	br	x21				// jump to kernel
 
 /*
@@ -93,4 +93,132 @@ copy_page:
 	sub     x8, x8, #16			// reduce copied bytes from size
 	cbnz    x8, 1b
 	ret
+
+_PreparePlatformHardware:
+	str     x30, [sp,#-16]!
+	bl      _ArmDisableBranchPrediction
+	bl      _ArmDisableInterrupts
+	bl      _ArmDisableAsynchronousAbort
+	bl      flush_cache_all
+	bl      _ArmDisableDataCache
+	bl      _ArmDisableInstructionCache
+	bl      _ArmDisableMmu
+	bl     	_ArmInvalidateTlb
+	mov     x0, xzr
+	ldr     x30, [sp],#16
+	ret
+
+_ArmDisableBranchPrediction:
+        ret
+
+_ArmDisableInterrupts:
+        msr     daifset, #0x3
+        isb
+        ret
+
+_ArmDisableAsynchronousAbort:
+        msr     daifset, #0x4
+        isb
+        ret
+/*
+ *	__flush_dcache_all()
+ *
+ *	Flush the whole D-cache.
+ *
+ *	Corrupted registers: x0-x7, x9-x11
+ */
+__flush_dcache_all:
+	dmb	sy				// ensure ordering with previous memory accesses
+	mrs	x0, clidr_el1			// read clidr
+	and	x3, x0, #0x7000000		// extract loc from clidr
+	lsr	x3, x3, #23			// left align loc bit field
+	cbz	x3, finished			// if loc is 0, then no need to clean
+	mov	x10, #0				// start clean at cache level 0
+loop1:
+	add	x2, x10, x10, lsr #1		// work out 3x current cache level
+	lsr	x1, x0, x2			// extract cache type bits from clidr
+	and	x1, x1, #7			// mask of the bits for current cache only
+	cmp	x1, #2				// see what cache we have at this level
+	b.lt	skip				// skip if no cache, or just i-cache
+	msr	csselr_el1, x10			// select current cache level in csselr
+	isb					// isb to sych the new cssr&csidr
+	mrs	x1, ccsidr_el1			// read the new ccsidr
+	and	x2, x1, #7			// extract the length of the cache lines
+	add	x2, x2, #4			// add 4 (line length offset)
+	mov	x4, #0x3ff
+	and	x4, x4, x1, lsr #3		// find maximum number on the way size
+	clz	w5, w4				// find bit position of way size increment
+	mov	x7, #0x7fff
+	and	x7, x7, x1, lsr #13		// extract max number of the index size
+loop2:
+	mov	x9, x4				// create working copy of max way size
+loop3:
+	lsl	x6, x9, x5
+	orr	x11, x10, x6			// factor way and cache number into x11
+	lsl	x6, x7, x2
+	orr	x11, x11, x6			// factor index number into x11
+	dc	cisw, x11			// clean & invalidate by set/way
+	subs	x9, x9, #1			// decrement the way
+	b.ge	loop3
+	subs	x7, x7, #1			// decrement the index
+	b.ge	loop2
+skip:
+	add	x10, x10, #2			// increment cache number
+	cmp	x3, x10
+	b.gt	loop1
+finished:
+	mov	x10, #0				// swith back to cache level 0
+	msr	csselr_el1, x10			// select current cache level in csselr
+	dsb	sy
+	isb
+	ret
+
+/*
+ *	flush_cache_all()
+ *
+ *	Flush the entire cache system.  The data cache flush is now achieved
+ *	using atomic clean / invalidates working outwards from L1 cache. This
+ *	is done using Set/Way based cache maintenance instructions.  The
+ *	instruction cache can still be invalidated back to the point of
+ *	unification in a single instruction.
+ */
+flush_cache_all:
+	str     x30, [sp,#-16]!
+	bl	__flush_dcache_all
+	mov	x0, #0
+	ic	ialluis				// I+BTB cache invalidate
+	ldr     x30, [sp],#16
+	ret
+
+_ArmDisableDataCache:
+        mrs     x0, sctlr_el1
+        and     x0, x0, #0xfffffffffffffffb
+        msr     sctlr_el1, x0
+        dsb     sy
+        isb
+        ret
+
+_ArmDisableInstructionCache:
+        mrs     x0, sctlr_el1
+        and     x0, x0, #0xffffffffffffefff
+        msr     sctlr_el1, x0
+        dsb     sy
+        isb
+        ret
+
+_ArmDisableMmu:
+        mrs     x0, sctlr_el1
+        and     x0, x0, #0xfffffffffffffffe
+        msr     sctlr_el1, x0
+        tlbi    vmalle1
+        dsb     sy
+        isb
+        ret
+
+_ArmInvalidateTlb:
+        tlbi    vmalle1
+        dsb     sy
+        isb
+        ret
+
 #endif
